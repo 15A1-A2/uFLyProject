0c0771ee754c3673b92bded5f8cea8bd2f4d536e
Erhun Akbas <Erhun@perronwei.nl>
2017-12-08 23:36:09 +0100

    login styling opgeruimt

--- application/view/login/index.php
+++ application/view/login/index.php
@@ -5,11 +5,11 @@
         <form action="<?php echo Config::get('URL'); ?>login/login" method="post">
           <ul class="tab-group">
             <li class="tab active"><a href="#login">Log In</a></li>
-            <li class="tab "><a href="<?php echo Config::get('URL'); ?>register/index">Sign up</a></li>
+            <li class="tab "><a href="<?php echo Config::get('URL'); ?>register/index">Registreer </a></li>
           </ul>
 
-            <input type="text" name="user_name" placeholder="Username or email" required />
-            <input type="password" name="user_password" placeholder="Password" autocomplete="off" required />
+            <input type="text" name="user_name" placeholder="Gebrukersnaam of e-mailadres" required />
+            <input type="password" name="user_password" placeholder="Wachtwoord" autocomplete="off" required />
 
              <?php if (!empty($this->redirect)) { ?>
                 <input type="hidden" name="redirect" value="<?php echo $this->encodeHTML($this->redirect); ?>" />
@@ -18,10 +18,10 @@
             <input type="hidden" name="csrf_token" value="<?= Csrf::makeToken(); ?>" />
             <input type="submit" class="submit-button" value="Log in"/>
           </form>
-         <!--  <div class="link-forgot-my-password">
-                  <a href="<?php echo Config::get('URL'); ?>login/requestPasswordReset">I forgot my password</a>
-          </div> -->
-          <!--  <label for="set_remember_me_cookie" class="remember-me-label">
+          <div class="link-forgot-my-password">
+            <a href="<?php echo Config::get('URL'); ?>login/requestPasswordReset">Ik ben mij wachtwoord vergeten</a>
+          </div>
+           <!-- <label for="set_remember_me_cookie" class="remember-me-label">
                      <input type="checkbox" name="set_remember_me_cookie" class="remember-me-checkbox" />
                      Remember me for 2 weeks
             </label> -->
--- application/view/register/index.php
+++ application/view/register/index.php
@@ -7,36 +7,33 @@
       <!-- register form -->
       <form method="post" action="<?php echo Config::get('URL'); ?>register/register_action">
         <!-- the user name input field uses a HTML5 pattern check -->
-        <input type="text" pattern="[a-zA-Z0-9,./!-]{2,64}" name="user_name" placeholder="Username (letters/numbers/special, min 10 chars)" required />
+        <input type="text" pattern="[a-zA-Z0-9,./!-]{2,64}" name="user_name" placeholder="Gebruikersnaam (letters/numbers/special)" required />
         <div class="outter">
           <div class="inner">
-            <input type="text" name="user_email" placeholder="Email address (a valid address)" required />
-            <input type="text" name="user_email_repeat" placeholder="Repeat Email address" required />
+            <input type="text" name="user_email" placeholder="E-mailadres (Geldige e-mailadres)" required />
+            <input type="text" name="user_email_repeat" placeholder="Herhaal e-mailadres" required />
           </div>
           <div class="inner two">
-            <input type="password" name="user_password_new" pattern=".{6,}" placeholder="Password (min 6 chars)" required autocomplete="off" />
-            <input type="password" name="user_password_repeat" pattern=".{6,}" required placeholder="Repeat Password" autocomplete="off" />
+            <input type="password" name="user_password_new" pattern=".{6,}" placeholder="Wachtwoord (min 6 chars)" required autocomplete="off" />
+            <input type="password" name="user_password_repeat" pattern=".{6,}" required placeholder="Herhaal Wachtwoord" autocomplete="off" />
           </div>
         </div>
 
 
         <!-- show the captcha by calling the login/showCaptcha-method in the src attribute of the img tag -->
         <img id="captcha" src="<?php echo Config::get('URL'); ?>register/showCaptcha" />
-        <input type="text" name="captcha" placeholder="Please enter above characters" required />
+        <input type="text" name="captcha" placeholder="Voer de characters hierboven in. " required />
 
         <!-- quick & dirty captcha reloader -->
         <a href="#" style="display: block; font-size: 11px; margin: 5px 0 15px 0; text-align: center"
-           onclick="document.getElementById('captcha').src = '<?php echo Config::get('URL'); ?>register/showCaptcha?' + Math.random(); return false">Reload Captcha</a>
+           onclick="document.getElementById('captcha').src = '<?php echo Config::get('URL'); ?>register/showCaptcha?' + Math.random(); return false">Herlaat Captcha</a>
         <input type="submit" value="Register" class="submit-button" />
       </form>
     </div>
   </div>
 </div>
-<!-- <div class="container">
-    <p style="display: block; font-size: 11px; color: #999;">
-        Please note: This captcha will be generated when the img tag requests the captcha-generation
-        (= a real image) from YOURURL/register/showcaptcha. As this is a client-side triggered request, a
-        $_SESSION["captcha"] dump will not show the captcha characters. The captcha generation
-        happens AFTER the request that generates THIS page has been finished.
-    </p>
-</div> -->
+
+<!-- Please note: This captcha will be generated when the img tag requests the captcha-generation
+(= a real image) from YOURURL/register/showcaptcha. As this is a client-side triggered request, a
+$_SESSION["captcha"] dump will not show the captcha characters. The captcha generation
+happens AFTER the request that generates THIS page has been finished. -->
