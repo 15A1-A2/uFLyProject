6f5b6433b92b1cc47c52e9fe1a601acaff281785
Glenn Ligthart <g.ligthart@mbp-van-glenn.home>
2017-12-12 18:53:15 +0100

    File_upload Check = file, type, file already exists

--- application/view/folder/index.php
+++ application/view/folder/index.php
@@ -1,46 +1,22 @@
-<nav class="navbar navbar-light bg-faded">
-	<section class="function-bar">
-	   <button class="btn btn-outline-success" type="button">New Folder</button>
-	   <button class="btn btn-outline-success" type="button">Downloaden</button>
-	   <button class="btn btn-outline-success" type="button">Delete</button>
-       <input class="btn btn-outline-success" type="file" name="file" />
-	</section>
-</nav>
-
-<form action="/file-upload" class="dropzone" id="my-awesome-dropzone">
-     
-</form>
-
-<div class="container">
-  <h1>FolderController/index</h1>
-    <div class="box">
-
-        <!-- echo out the system feedback (error and success messages) -->
-        <?php $this->renderFeedbackMessages(); ?>
-
-        <h3>What happens here ?</h3>
-        <p>
-            Here is the user suppose to create New Folders, change the name, open the folder, upload data to the folder, delete folders, delete data.
-        </p>
-
+<div class="box">
+  <!-- echo out the system feedback (error and success messages) -->
+  <?php $this->renderFeedbackMessages(); ?>
+  <div class="row">
+    <div class="col-md-12">
+      <div class="card">
+        <div class="card-header" data-background-color="red">
+          <h4 class="title">Organisation Folder</h4>
+            <p class="category">Here you see your domain folder</p>  
+        </div>
+          <form action="/file-upload" class="dropzone" id="my-awesome-dropzone"></form> 
+      </div>
+      <div class="col-md-12">
+        <h4>Here are the files being stored</h4>
+          <form action="FolderModel.php" method="post" enctype="multipart/form-data">
+            <input type="file" name="uploadFile" id="uploadFile">
+            <input type="submit" value="Upload File" name="submit">
+          </form>      
+      </div>
     </div>
-
-    <?php 
-        $curdir = getcwd();
-
-        if (mkdir($curdir . "/new folder2" , 0777)) {
-            echo "folder created";
-        } else {
-            echo "failed";
-        }
-    ?>
-
-    <form method="post" name="folder"  action="php_folder.php">
-      <p>
-        <input type="text" name="folder" id="folder"> 
-        Create folder_name</p>
-      <p>
-        <input type="submit" name="mkdir" id="button" value="Submit">
-      </p>
-    </form>
+  </div>
 </div>
